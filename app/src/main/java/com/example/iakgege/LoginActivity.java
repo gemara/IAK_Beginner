d84b3d5733cf02cc5d1f36923a5cc7427d40b652
gemara <gemara1982@gmail.com>
2018-01-28 16:15:49 +0700

    MainActivity get Intent

--- app/src/main/java/com/example/iakgege/LoginActivity.java
+++ app/src/main/java/com/example/iakgege/LoginActivity.java
@@ -1,13 +1,69 @@
 package com.example.iakgege;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText et_username;
+    private EditText et_password;
+    private Button btn_cancel;
+    private Button btn_login;
+
+    private static final String USERNAME = "admin";
+    private static final String PASSWORD = "admin";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        et_username = findViewById(R.id.et_username);
+        et_password = findViewById(R.id.et_password);
+        btn_cancel = findViewById(R.id.btn_cancel);
+        btn_login = findViewById(R.id.btn_login);
+
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(LoginActivity.this, "Button Ditekan", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = et_username.getText().toString();
+                String password = et_password.getText().toString();
+
+                if (TextUtils.isEmpty(username)){
+                    et_username.setError("Ga Boleh Kosong Beul!");
+                }
+                if (TextUtils.isEmpty(password)){
+                    et_password.setError("Ga Boleh Kosong Juga Beul!");
+                }
+                if (username.equals(USERNAME) && password.equals(PASSWORD))  {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.putExtra(MainActivity.USERNAME, username);
+                    intent.putExtra(MainActivity.PASSWORD, password);
+
+                    finish();
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(LoginActivity.this, "JANGAN MACEM MACEM!", Toast.LENGTH_SHORT).show();
+                    et_username.getText().clear();
+                    et_password.getText().clear();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/iakgege/MainActivity.java
+++ app/src/main/java/com/example/iakgege/MainActivity.java
@@ -4,10 +4,17 @@ import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static String TAG = MainActivity.class.getSimpleName();
+
+    public final static String USERNAME = "username";
+    public final static String PASSWORD = "password";
+
+    private TextView tv_username;
+    private TextView tv_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +22,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.d(TAG, "onCreate: ");
+
+        tv_username = findViewById(R.id.tv_username);
+        tv_password = findViewById(R.id.tv_password);
+
+        String username_intent = getIntent().getStringExtra(USERNAME);
+        String password_intent = getIntent().getStringExtra(PASSWORD);
+
+        tv_username.setText(username_intent);
+        tv_password.setText(password_intent);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+
 
         Log.d(TAG, "onStart: ");
     }
